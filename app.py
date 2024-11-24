69354d4f74419d05bd0f3a1a9b0562d7c3d6ad10
soichiro <sss738223@gmail.com>
2024-11-24 23:08:28 +0900

    コミット

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # CORS設定を更新
+
+
+CORS(app, resources={
+    r"/api/*": {
+        "origins": [
+            "https://tech0-gen-8-step3-testapp-node2-30.azurewebsites.net",
+            "https://localhost:3000" # ローカル開発⽤
+            ]
+        }
+    })  # CORS設定を更新
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -30,5 +39,16 @@ def echo():
     message = data.get('message', 'No message provided')
     return jsonify({"message": f"echo: {message}"})
 
+@app.route('/robots933456.txt', methods=['GET'])
+def robots():
+    return "User-agent: *\nDisallow:", 200, {'Content-Type': 'text/plain'}
+
+@app.route('/health', methods=['GET'])
+def health():
+    return "OK", 200
+
 if __name__ == '__main__':
-    app.run(debug=True)
+ # 環境変数 PORT を取得（デフォルトは 8000）
+    port = int(os.environ.get('PORT', 8000))
+ # デバッグモードをローカル環境では有効に、本番では無効に
+    app.run(host='0.0.0.0', port=port, debug=False)
