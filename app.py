3b98b7d5f4ba4195fc0a21bfefd1942e1f07f2c2
soichiro <sss738223@gmail.com>
2024-11-24 18:04:33 +0900

    コミット

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # CORS設定を更新
